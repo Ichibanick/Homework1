aac9a32b6e7cf6d6ea8ff8ca8d50c9e6674fee7f
Shikalov Nikita <muchik.33@yandex.ru>
2023-08-17 13:03:30 +0300

    задания 3, 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее
+
+// Console.Write(" Введите число a =  ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число b =  ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//     Console.WriteLine("Число a > b ");
+// }
+// else if(num1 == num2)
+// {
+//     Console.WriteLine("Числа равны ");
+// }
+// else
+// {
+//     Console.WriteLine("Число b > a ");
+// }
+
+
+
+
+// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// Console.Write(" Введите число a =  ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число b =  ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число c =  ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if ( num1 > num2 && num1 > num3)
+// {
+//     Console.WriteLine("Максимальное число: ");
+//     Console.Write(num1);
+// }
+// else if ( num2 > num1 && num2 > num3)
+// {
+//     Console.WriteLine("Максимальное число: ");
+//     Console.Write(num2);
+// }
+// else if ( num3 > num1 && num3 > num2)
+// {
+//     Console.WriteLine("Максимальное число: ");
+//     Console.Write(num3);
+// }
+// else if ( num3 > num1 && num3 > num2)
+// {
+//     Console.WriteLine("Максимальное число: "); Console.Write(num3);
+// }
+// else if ( num3 == num1 && num3 == num2)
+// {
+//     Console.WriteLine("Максимального числа нет, все числа равны: ");
+//     Console.Write(num1);
+// }
+
+
+
+
+// Напи
